804bf9af79a9906c00c0528ac7cc108d753f5f99
Galileu Santos <galilasmb@gmail.com>
2022-02-01 18:27:46 -0300

    Alteração nos arquivos de entrada para validação

--- ficha_individual_validate.py
+++ ficha_individual_validate.py
@@ -2,16 +2,16 @@ import xmlschema
 import json
 from xml.etree.ElementTree import ElementTree
 
-my_schema = xmlschema.XMLSchema('XSD/fichaatendimentoindividualmaster.xsd')
+my_schema = xmlschema.XMLSchema('xml_examples/schema.xsd')
 # my_schema = xmlschema.XMLSchema('https://github.com/laboratoriobridge/esusab-integracao/blob/0c16a48ebda2ae65ccc0bbdca506f3ad5e2ea6c1/XSD/fichaatendimentoindividualmaster.xsd')
 
 
-is_valid = my_schema.is_valid('xml_examples/Atendimento_individual.esus.xml')
+is_valid = my_schema.is_valid('xml_examples/sample.xml')
 
 
 print(f"Schema válido: {is_valid}")
 
 if not is_valid:
     print(f"Erros de validação do Schema \n")
-    print(my_schema.validate('xml_examples/Atendimento_individual.esus.xml'))
+#    print(my_schema.validate('xml_examples/Cadastro_Domiciliar.esus.xml'))
 
