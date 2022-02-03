42110bd40235cfca6d7974b299f260f683ad08e9
Mayara Barbosa <mayara.barbosa@lais.huol.ufrn.br>
2022-02-03 17:55:57 -0300

    Teste com ficha de atendimento master

--- ficha_individual_validate.py
+++ ficha_individual_validate.py
@@ -12,5 +12,17 @@ print(f"Schema válido: {is_valid}")
 
 if not is_valid:
     print(f"Erros de validação do Schema \n")
-    print(my_schema.validate('xml_examples/Cadastro_Domiciliar.esus.xml'))
+    print(my_schema.validate('xml_examples/Atendimento_individual.esus.xml'))
+
+
+# Ficha de Atendimento Invidual Master
+my_schema = xmlschema.XMLSchema('XSD/fichaatendimentoindividualmaster.xsd')
+
+is_valid = my_schema.is_valid('xml_examples/AtendimentoIndividualMaster.esus.xml')
+
+print(f"Schema válido: {is_valid}")
+
+if not is_valid:
+    print(f"Erros de validação do Schema \n")
+    print(my_schema.validate('xml_examples/AtendimentoIndividualMaster.esus.xml'))
 
